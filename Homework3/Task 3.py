fa3737aa39f0328a906ffea60aa239f08a0f2f84
Yurii Korotchuk <yuriikorotchuk@MacBook-Pro-Yurii.local>
2022-06-26 18:12:09 +0300

    change homework

--- Homework3/Task 3.py
+++ Homework3/Task 3.py
@@ -1,27 +1,16 @@
-
-# Task 2 Вести з консолі строку зі слів (або скористайтеся константою).
-# Напишіть код, який визначить кількість слів, в цих даних.
-
-# test = input("Введіть набір слів: ")
-# result = test.split(" ")
-#
-# if test.isspace():
-#     print("Поле не повинно бути пустим!!!")
-# else:
-#     print(len(result))
-
-
-
 # Task 3 Існує ліст з різними даними, наприклад
 # lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum'].
 # Напишіть механізм, який сформує новий list (наприклад lst2), який би містив всі числові змінні, які є в lst1.
 # Майте на увазі, що данні в lst1 не є статичними можуть змінюватись від запуску до запуску.
 
+from rich import print
+
 lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
 lst2 = lst1.copy()
 
 for i in lst1:
-    if type(i) != int:
+    if type(i) != int and type(i) != float:
         lst2.remove(i)
-print(f"List #2: {lst2}")
 print(lst1)
+print(f"List #2: :Ukraine: {lst2} :Ukraine:")
+
