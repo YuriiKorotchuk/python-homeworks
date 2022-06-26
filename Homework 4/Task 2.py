dbd3df379e083e396e1fe9e7d5f463507ff6fbb5
Yurii Korotchuk <yuriikorotchuk@MacBook-Pro-Yurii.local>
2022-06-26 19:55:51 +0300

    Ðhomework 4

---
+++ Homework 4/Task 2.py
@@ -0,0 +1,32 @@
+# Task 2 - Є два довільних числа які відповідають за мінімальну і максимальну ціну.
+# Є Dict з назвами магазинів і цінами: { "cilpio": 47.999, "a_studio" 42.999, "momo": 49.999, "main-service":
+# 37.245, "buy.ua": 38.324, "my-store": 37.166, "the_partner": 38.988, "sto": 37.720, "rozetka": 38.003}.
+# Напишіть код, який знайде і виведе на екран назви магазинів, ціни яких попадають в діапазон між мінімальною
+# і максимальною ціною. Наприклад:
+# lower_limit = 35.9
+# upper_limit = 37.339
+# > match: "my-store", "main-service"
+
+
+my_dict = {"cilpio": 47.999,
+           "a_studio": 42.999,
+           "momo": 49.999,
+           "main-service": 37.245,
+           "buy.ua": 38.324,
+           "my-store": 37.166,
+           "the_partner": 38.988,
+           "sto": 37.720,
+           "rozetka": 38.003,
+           }
+try:
+    lower_limit = float(input("Введіть мінімальну ціну: "))
+    upper_limit = float(input("Введіть максимальну ціну: "))
+except ValueError as e:
+    print("Ви написали не валідне значення!!!")
+else:
+    if lower_limit > upper_limit:
+        print("Ціна нижнього ліміту не повинна бути більша чим ціна верхнього!!!")
+    else:
+        for shop, price in my_dict.items():
+            if lower_limit <= price <= upper_limit:
+                print("Назвa магазину: " + shop)
